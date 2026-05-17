a850fb96f63a49b81fe530f00bd4128bf565d302
gemilmejia <gemilmejia64@gmail.com>
2026-05-18 00:20:40 +0800

    fix: Update contact.component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,32 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-interface ContactForm {
-  name: string;
-  email: string;
-  subject: string;
-  message: string;
-}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
-  @Input() contact: ContactForm = {
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  };
-
-  @Output() submitForm = new EventEmitter<any>();
-
-  onSubmit(form: any): void {
-    this.submitForm.emit(form);
-  }
-}
+export class ContactComponent {}
