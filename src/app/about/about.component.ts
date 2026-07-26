e1938a7bea6a833bb6498dd2f14ee400d2bbc5df
gemilmejia <gemilmejia64@gmail.com>
2026-07-26 23:30:15 +0800

    feat: Update list of skills in typescript

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,4 +8,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+
+frontendSkills: string[] = [
+  'HTML',
+  'CSS',
+  'Javascript',
+  'Angular',
+  'Bootstrap',
+  'Tailwind CSS',
+  'ReactJS'
+];
+
+backendSkills: string[] = [
+  'C',
+  'C++',
+  'PHP',
+  'Python',
+  'Laravel',
+  'C#',
+  'ASP.NET',
+  'RestAPI Basics',
+  'Node.js',
+  'Express.js'
+];
+
+databaseSkills: string[] = [
+  'SQL',
+  'MySQL',
+  'SQL Server Management Studio',
+];
+
+otherToolsSkills: string[] = [
+  'Git',
+  'GitHub',
+  'Figma',
+  'Adobe Photoshop',
+  'Adobe Premiere'
+];
+}
